5d414a7d7fd7bc5255eb78c839e7827a032153a5
webLion200 <794358525@qq.com>
2017-11-23 22:18:20 +0800

    mapStateToProps你要什么属性放到组件中, actionCreator你要什么方法放到组件中，会自动dispatch

--- src/components/App.js
+++ src/components/App.js
@@ -23,6 +23,8 @@ const mapStateToProps = (state) => ({
 const actionCreator = {increment, decrement, incrementAsync};       //  mapPropsToStatus
 
 export default connect(
+    // 你要什么属性放到组件中
     mapStateToProps,
+    // 你要什么方法放到组件中，会自动dispatch
     actionCreator
 )(App);
